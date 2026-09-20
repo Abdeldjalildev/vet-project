86eb9e87a8e59ea2fdaa3c7a71a0c90ec8e7a8cd
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-20 16:07:58 +0100

    feat(phase-9): load reliability and accessibility translations

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './i18n/phase5'
 import './i18n/phase6'
 import './i18n/phase7'
 import './i18n/phase8'
+import './i18n/phase9'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider'
 
