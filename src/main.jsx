c154f85b5649959a0594f3a400f323d83ef39dfc
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-20 15:25:13 +0100

    feat(phase-4): load clinic admin translations

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n/phase2'
+import './i18n/phase4'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider'
 
