3534d9b2935db242c8adaba9d192a92059ef83f1
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-20 14:51:50 +0100

    refactor(firestore): expose centralized document and collection refs

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -1,4 +1,23 @@
-import { getFirestore } from 'firebase/firestore'
+import { collection, doc, getFirestore } from 'firebase/firestore'
 import { firebaseApp } from './firebase'
 
 export const firestore = getFirestore(firebaseApp)
+
+export const clinicRef = (clinicId) => doc(firestore, 'clinics', clinicId)
+
+export const userRef = (uid) => doc(firestore, 'users', uid)
+
+export const clinicServicesRef = (clinicId) =>
+  collection(firestore, 'clinics', clinicId, 'services')
+
+export const clinicFaqsRef = (clinicId) =>
+  collection(firestore, 'clinics', clinicId, 'faqs')
+
+export const clinicAppointmentsRef = (clinicId) =>
+  collection(firestore, 'clinics', clinicId, 'appointments')
+
+export const clinicAnalyticsEventsRef = (clinicId) =>
+  collection(firestore, 'clinics', clinicId, 'analyticsEvents')
+
+export const clinicAnalyticsAggregatesRef = (clinicId) =>
+  collection(firestore, 'clinics', clinicId, 'analyticsAggregates')
