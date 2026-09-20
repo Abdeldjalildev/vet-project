3000b36bb01ba8404f7a083464f587753dfa74f7
KHALFA abdeldjalil <abdeldjalil.khalfa@enssmal.edu.dz>
2026-09-20 15:40:57 +0100

    feat(phase-5): load clinic configuration translations

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n/phase2'
 import './i18n/phase4'
+import './i18n/phase5'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider'
 
